0ad546cb901c91155de2f88d7b72e781a8823234
Catherine Noll <noll.catherine@gmail.com>
2018-05-19 02:47:39 -0400

    Invoicer admin

--- invoicer/admin.py
+++ invoicer/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (Client,
+                     ClientGroup,
+                     Invoice,
+                     Project,
+                     ProjectStatus,
+                     ProjectType,
+                     Task,
+                     TaskStatus,
+                     TaskType,
+                     Work)
+
+
+admin.site.register(Client)
+admin.site.register(ClientGroup)
+admin.site.register(Invoice)
+admin.site.register(Project)
+admin.site.register(ProjectStatus)
+admin.site.register(ProjectType)
+admin.site.register(Task)
+admin.site.register(TaskStatus)
+admin.site.register(TaskType)
+admin.site.register(Work)
