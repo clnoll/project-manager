36fd5523f82b11a0024e6fe08091da96340692b1
Catherine Noll <noll.catherine@gmail.com>
2018-06-22 18:02:52 +0200

    Add input fields to invoice form

--- invoicer/forms/invoice_form.py
+++ invoicer/forms/invoice_form.py
@@ -1,6 +1,10 @@
-from django import forms
+import re
+from collections import defaultdict
 
-from invoicer.models import Invoice, TaskItem
+from django import forms
+from django.db import transaction
+
+from invoicer.models import Invoice, Task, TaskItem
 
 
 class InvoiceForm(forms.ModelForm):
@@ -11,6 +15,39 @@ class InvoiceForm(forms.ModelForm):
 
     def save(self, project, *args, **kwargs):
         invoice = super().save(*args, **kwargs)
-        task_item_ids = [int(k.lstrip('task_item_')) for k in self.data.dict().keys() if 'task_item_' in k]
-        TaskItem.objects.filter(pk__in=task_item_ids).update(invoice_id=invoice.id)
+
+        data = self.data.dict()
+        task_names_to_update = {int(k.lstrip('task_name_')): v for k, v in data.items()
+                                if 'task_name_' in k}
+
+        taskitems_to_update = defaultdict(dict)
+
+        for k, v in data.items():
+            if 'taskitem_' in k:
+                pattern = r'taskitem_([a-z0-9_]*)_([0-9]*)'
+                taskitem = re.match(pattern, k)
+                if taskitem:
+                    key, taskitem_id = taskitem.groups()
+                    taskitems_to_update[int(taskitem_id)][key] = v
+
+        taskitem_ids_to_invoice = [int(k.lstrip('invoice_')) for k in data.keys()
+                                   if 'invoice_' in k]
+
+        with transaction.atomic():
+            for task_id, task_name in task_names_to_update.items():
+                task = Task.objects.get(pk=task_id)
+                if task:
+                    task.name = task_name
+                    task.save()
+
+            for taskitem_id, taskitem_kvs in taskitems_to_update.items():
+                taskitem = TaskItem.objects.get(pk=taskitem_id)
+                if taskitem:
+                    for k, v in taskitem_kvs.items():
+                        if hasattr(taskitem, k):
+                            setattr(taskitem, k, v)
+                taskitem.save()
+
+            TaskItem.objects.filter(pk__in=taskitem_ids_to_invoice).update(invoice_id=invoice.id)
+
         return invoice
